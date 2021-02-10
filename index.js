4b00ff2a3a42116762d04ef9758495ed9ee0e159
mm <maciej.markiewicz10@gmail.com>
2021-02-10 18:16:54 +0100

    ip show

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 let metaServerModule=require('./public/metaServer')
 
+var ip = require("ip");
+console.dir ( ip.address() );
+
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('addr: ' + add);
+})
+
 
 let metaServer=new metaServerModule.MetaServer();
 
