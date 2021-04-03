e090a44dba7f77fda61ddc656748f6decf211d02
mm <maciej.markiewicz10@gmail.com>
2021-04-03 23:11:31 +0200

    bunnies dont clip now

--- GLOBAL.js
+++ GLOBAL.js
@@ -1,6 +1,6 @@
 const GLOBAL = {
-    // BASE_URL: "https://bunnies71.herokuapp.com",
-    BASE_URL: "http://127.0.0.1:8080",
+    BASE_URL: "https://bunnies71.herokuapp.com",
+    // BASE_URL: "http://127.0.0.1:8080",
     ROUND_LENGTH_SECONDS: 60,
     ROUNDS_MAX: 7,
 }
